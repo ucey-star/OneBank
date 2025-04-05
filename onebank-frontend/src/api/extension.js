40c84814db85b4bf257e6b5a500bc72072d6e254
Uchechukwu Unanka <uche@uni.minerva.edu>
2025-04-05 15:38:03 -0700

    final push

--- onebank-frontend/src/api/extension.js
+++ onebank-frontend/src/api/extension.js
@@ -1,12 +1,3 @@
-if (!process.env.REACT_APP_API_BASE_URL) {
-    throw new Error(
-      "Missing REACT_APP_API_BASE_URL in the environment variables. Please set it and rebuild."
-    );
-  }
-  
-  // Use the environment variable directly
-  const BASE_URL = process.env.REACT_APP_API_BASE_URL;
-
 export async function downloadExtension() {
     window.location.href = "https://chromewebstore.google.com/detail/one-bank/mcphffopiondckeljgldjknjiicdhajd";
 }
