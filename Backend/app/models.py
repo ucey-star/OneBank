708810841f5fe297d7d6a1ca8891c2176fea9e08
Uchechukwu Unanka <uche@uni.minerva.edu>
2025-04-05 19:29:41 -0700

    fix

--- Backend/app/models.py
+++ Backend/app/models.py
@@ -19,7 +19,7 @@ class User(db.Model, UserMixin):
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password, method='pbkdf2:sha256', salt_length=16)
-        
+
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
@@ -34,7 +34,7 @@ class CreditCard(db.Model):
     expiry_date = db.Column(db.String(10), nullable=True)  # Format: MM/YY
     cvv = db.Column(db.String(4), nullable=True) 
     issuer = db.Column(db.String(50), nullable=False)  # e.g., Visa, MasterCard, Amex
-    card_type = db.Column(db.String(50), nullable=False)  # e.g., Debit, Credit, Prepaid
+    card_type = db.Column(db.String(100), nullable=False)  # e.g., Debit, Credit, Prepaid
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     socialized_benefits = db.Column(db.Text, nullable=True)
 
