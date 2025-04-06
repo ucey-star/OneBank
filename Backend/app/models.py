3d90a3951e9a8100f079993dedd4232e71d26036
Uchechukwu Unanka <uche@uni.minerva.edu>
2025-04-05 18:10:09 -0700

    fix

--- Backend/app/models.py
+++ Backend/app/models.py
@@ -18,8 +18,8 @@ class User(db.Model, UserMixin):
 
 
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
+        self.password_hash = generate_password_hash(password, method='pbkdf2:sha256', salt_length=16)
+        
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
