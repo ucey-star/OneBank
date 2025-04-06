341b2e8fdc11078590e000c5961f35409d3ecf46
Uchechukwu Unanka <uche@uni.minerva.edu>
2025-04-05 17:52:24 -0700

    :fix

--- Backend/app/models.py
+++ Backend/app/models.py
@@ -9,7 +9,7 @@ class User(db.Model, UserMixin):
     first_name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(1024))
     default_reward_type = db.Column(db.String(20), default="cashback")
      # Add a new column for storing a profile picture URL or file path
     profile_pic = db.Column(db.LargeBinary, nullable=True)
